56cb6c141ad35714b013c8f2f0011e67562a55b4
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2025-08-25 09:55:09 +0200

    findJobiboxCities util

--- src/components/findJobiBox/jobiMap/index.tsx
+++ src/components/findJobiBox/jobiMap/index.tsx
@@ -1,4 +1,5 @@
 import { $, component$, useSignal, type Signal } from "@builder.io/qwik";
+import { regionsData } from "~/utils/findJobiboxCities";
 
 interface ModalFindJobiBoxProps {
   isOpen: Signal<boolean>;
@@ -7,88 +8,6 @@ interface ModalFindJobiBoxProps {
 export const JobiMap = component$<ModalFindJobiBoxProps>(({ isOpen }) => {
   const selectedRegion = useSignal<string | null>(null);
   const hoveredRegion = useSignal<string | null>(null);
-
-  const regionsData = {
-    A: {
-      name: "Grand Est",
-      cities: ["Troyes", "Nancy"],
-    },
-    B: {
-      name: "Nouvelle-Aquitaine",
-      cities: ["Bordeaux", "Limoges", "La Rochelle"],
-    },
-    C: {
-      name: "Auvergne-Rhône-Alpes",
-      cities: ["Lyon", "Villeurbanne"],
-    },
-    D: {
-      name: "Bourgogne-Franche-Comté",
-      cities: [],
-    },
-    E: {
-      name: "Bretagne",
-      cities: [],
-    },
-    F: {
-      name: "Centre-Val de Loire",
-      cities: ["Orléans", "Rocheplatte"],
-    },
-    G: {
-      name: "Corse",
-      cities: [],
-    },
-    H: {
-      name: "Île-de-France",
-      cities: ["Paris et ses arrondissements"],
-    },
-    I: {
-      name: "Occitanie",
-      cities: ["Toulouse", "Montpellier"],
-    },
-    J: {
-      name: "Hauts-de-France",
-      cities: [
-        "Valenciennes",
-        "Dunkerque",
-        "Douai",
-        "Lens",
-        "Tourcoing",
-        "Maubeuge",
-        "Villeneuve d'ascq",
-        "Cuincy",
-        "La Madeleine",
-        "Béthune",
-        "Amiens",
-        "Saint-Quentin",
-        "Laon",
-        "Creil",
-        "Hazebrouck",
-        "Boulogne-sur-Mer",
-        "Provin",
-        "Saint-Omer",
-        "Roubaix",
-        "Lesquin",
-        "Prouvy",
-        "Denain",
-        "Bailleul",
-        "Calais",
-        "Armentières",
-        "Cambrai",
-      ],
-    },
-    K: {
-      name: "Normandie",
-      cities: ["Dieppe", "Friville-Escarbotin"],
-    },
-    L: {
-      name: "Pays de la Loire",
-      cities: ["Nantes", "Saint-Nazaire"],
-    },
-    M: {
-      name: "Provence-Alpes-Côte d'Azur",
-      cities: [],
-    },
-  };
 
   const handleRegionClick = $((regionId: string) => {
     selectedRegion.value = regionId;
---
+++ src/utils/findJobiboxCities.ts
@@ -0,0 +1,81 @@
+export const regionsData = {
+  A: {
+    name: "Grand Est",
+    cities: ["Troyes", "Nancy"],
+  },
+  B: {
+    name: "Nouvelle-Aquitaine",
+    cities: ["Bordeaux", "Limoges", "La Rochelle"],
+  },
+  C: {
+    name: "Auvergne-Rhône-Alpes",
+    cities: ["Lyon", "Villeurbanne"],
+  },
+  D: {
+    name: "Bourgogne-Franche-Comté",
+    cities: [],
+  },
+  E: {
+    name: "Bretagne",
+    cities: [],
+  },
+  F: {
+    name: "Centre-Val de Loire",
+    cities: ["Orléans", "Rocheplatte"],
+  },
+  G: {
+    name: "Corse",
+    cities: [],
+  },
+  H: {
+    name: "Île-de-France",
+    cities: ["Paris et ses arrondissements"],
+  },
+  I: {
+    name: "Occitanie",
+    cities: ["Toulouse", "Montpellier"],
+  },
+  J: {
+    name: "Hauts-de-France",
+    cities: [
+      "Valenciennes",
+      "Dunkerque",
+      "Douai",
+      "Lens",
+      "Tourcoing",
+      "Maubeuge",
+      "Villeneuve d'ascq",
+      "Cuincy",
+      "La Madeleine",
+      "Béthune",
+      "Amiens",
+      "Saint-Quentin",
+      "Laon",
+      "Creil",
+      "Hazebrouck",
+      "Boulogne-sur-Mer",
+      "Provin",
+      "Saint-Omer",
+      "Roubaix",
+      "Lesquin",
+      "Prouvy",
+      "Denain",
+      "Bailleul",
+      "Calais",
+      "Armentières",
+      "Cambrai",
+    ],
+  },
+  K: {
+    name: "Normandie",
+    cities: ["Dieppe", "Friville-Escarbotin"],
+  },
+  L: {
+    name: "Pays de la Loire",
+    cities: ["Nantes", "Saint-Nazaire"],
+  },
+  M: {
+    name: "Provence-Alpes-Côte d'Azur",
+    cities: [],
+  },
+};
