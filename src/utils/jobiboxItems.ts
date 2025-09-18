58b356c29de07e12b09545f42120957e9cfa0072
Alexis Moren <alexismoren62@hotmail.com>
2025-09-18 18:17:09 +0200

    compress img

--- src/utils/jobiboxItems.ts
+++ src/utils/jobiboxItems.ts
@@ -1,6 +1,13 @@
-import Img1 from "../assets/carrousel1.avif";
-import Img2 from "../assets/carrousel2.avif";
-import Img3 from "../assets/carrousel3.avif";
+import Img1 from "../assets/jobiboxItems/carrousel1.avif";
+import Img2 from "../assets/jobiboxItems/carrousel2.avif";
+import Img3 from "../assets/jobiboxItems/carrousel3.avif";
+import Img4 from "../assets/jobiboxItems/carrousel4.avif";
+import Img5 from "../assets/jobiboxItems/carrousel5.avif";
+import Img6 from "../assets/jobiboxItems/carrousel6.avif";
+import Img7 from "../assets/jobiboxItems/carrousel7.avif";
+import Img8 from "../assets/jobiboxItems/carrousel8.avif";
+import Img9 from "../assets/jobiboxItems/carrousel9.avif";
+import Img10 from "../assets/jobiboxItems/carrousel10.avif";
 
 export const jobiboxItems = [
     {
@@ -25,6 +32,55 @@ export const jobiboxItems = [
         gradient: "linear-gradient(180deg, rgba(113, 64, 60, 0) 0%, #71403C 90%)",
     },
     {
+        src: Img4,
+        alt: "Fun Radio",
+        title: "Fun Radio",
+        subtitle: "Studio Ibiza Experience",
+        gradient: "linear-gradient(180deg, rgba(238, 46, 139, 0) 0%, #EE2E8B 90%)",
+    },
+    {
+        src: Img5,
+        alt: "M6",
+        title: "M6",
+        subtitle: "L'amour est dans le pré",
+        gradient: "linear-gradient(180deg, rgba(79, 114, 81, 0) 0%, #5B7A5B 90%)",
+    },
+    {
+        src: Img6,
+        alt: "Racing coeur de Lens",
+        title: "Racing coeur de Lens",
+        subtitle: "Booste votre carrière",
+        gradient: "linear-gradient(180deg, rgba(113, 64, 60, 0) 0%, #71403C 90%)",
+    },
+    {
+        src: Img7,
+        alt: "Fun Radio",
+        title: "Fun Radio",
+        subtitle: "Studio Ibiza Experience",
+        gradient: "linear-gradient(180deg, rgba(238, 46, 139, 0) 0%, #EE2E8B 90%)",
+    },
+    {
+        src: Img8,
+        alt: "M6",
+        title: "M6",
+        subtitle: "L'amour est dans le pré",
+        gradient: "linear-gradient(180deg, rgba(79, 114, 81, 0) 0%, #5B7A5B 90%)",
+    },
+    {
+        src: Img9,
+        alt: "Racing coeur de Lens",
+        title: "Racing coeur de Lens",
+        subtitle: "Booste votre carrière",
+        gradient: "linear-gradient(180deg, rgba(113, 64, 60, 0) 0%, #71403C 90%)",
+    },
+    {
+        src: Img10,
+        alt: "Fun Radio",
+        title: "Fun Radio",
+        subtitle: "Studio Ibiza Experience",
+        gradient: "linear-gradient(180deg, rgba(238, 46, 139, 0) 0%, #EE2E8B 90%)",
+    },
+    {
         src: Img1,
         alt: "Fun Radio",
         title: "Fun Radio",
@@ -46,45 +102,52 @@ export const jobiboxItems = [
         gradient: "linear-gradient(180deg, rgba(113, 64, 60, 0) 0%, #71403C 90%)",
     },
     {
-        src: Img1,
+        src: Img4,
         alt: "Fun Radio",
         title: "Fun Radio",
         subtitle: "Studio Ibiza Experience",
         gradient: "linear-gradient(180deg, rgba(238, 46, 139, 0) 0%, #EE2E8B 90%)",
     },
     {
-        src: Img2,
+        src: Img5,
         alt: "M6",
         title: "M6",
         subtitle: "L'amour est dans le pré",
         gradient: "linear-gradient(180deg, rgba(79, 114, 81, 0) 0%, #5B7A5B 90%)",
     },
     {
-        src: Img3,
+        src: Img6,
         alt: "Racing coeur de Lens",
         title: "Racing coeur de Lens",
         subtitle: "Booste votre carrière",
         gradient: "linear-gradient(180deg, rgba(113, 64, 60, 0) 0%, #71403C 90%)",
     },
     {
-        src: Img1,
+        src: Img7,
         alt: "Fun Radio",
         title: "Fun Radio",
         subtitle: "Studio Ibiza Experience",
         gradient: "linear-gradient(180deg, rgba(238, 46, 139, 0) 0%, #EE2E8B 90%)",
     },
     {
-        src: Img2,
+        src: Img8,
         alt: "M6",
         title: "M6",
         subtitle: "L'amour est dans le pré",
         gradient: "linear-gradient(180deg, rgba(79, 114, 81, 0) 0%, #5B7A5B 90%)",
     },
     {
-        src: Img3,
+        src: Img9,
         alt: "Racing coeur de Lens",
         title: "Racing coeur de Lens",
         subtitle: "Booste votre carrière",
         gradient: "linear-gradient(180deg, rgba(113, 64, 60, 0) 0%, #71403C 90%)",
+    },
+    {
+        src: Img10,
+        alt: "Fun Radio",
+        title: "Fun Radio",
+        subtitle: "Studio Ibiza Experience",
+        gradient: "linear-gradient(180deg, rgba(238, 46, 139, 0) 0%, #EE2E8B 90%)",
     },
 ];
