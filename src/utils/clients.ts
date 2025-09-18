7e854c30a97299007f279ce4fa249be86c6f15b7
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2025-09-18 13:34:55 +0200

    clients logos

--- src/utils/clients.ts
+++ src/utils/clients.ts
@@ -1,56 +1,53 @@
-import logoFranceTravail from "../assets/clients/france.avif";
-import logoDirectFormation from "../assets/clients/formation.png";
-import logoAssifep from "../assets/clients/assifep.png";
-import logoFunRadio from "../assets/clients/client-fun-radio.webp";
-import logoM6 from "../assets/clients/m6.avif";
-import campus from "../assets/clients/campus.webp";
-import rcl from "../assets/clients/rcl.avif";
-import crit from "../assets/clients/crit.png";
-import elevup from "../assets/clients/elevup.avif";
-import partnaire from "../assets/clients/partnaire.webp";
-import adelie from "../assets/clients/adelie.png";
-import leclerc from "../assets/clients/leclerc.webp";
-import synergie from "../assets/clients/synergie.webp";
-import actual from "../assets/clients/actual.jpg";
-import laho from "../assets/clients/laho.avif";
+import logoActual from "../assets/clients/logo_actual.webp";
+import logoAdelie from "../assets/clients/logo_adelie.webp";
+import logoAfpiDunkerque from "../assets/clients/logo_afpi_dunkerque.webp";
+import logoAssifep from "../assets/clients/logo_assifep.webp";
+import logoCciGrandNancy from "../assets/clients/logo_cci_grand_nancy_metropole.webp";
+import logoDirectFormation from "../assets/clients/logo_directt_formation.webp";
+import logoEfpBruxelles from "../assets/clients/logo_efp_bruxelles.webp";
+import logoElevup from "../assets/clients/logo_elevup.webp";
+import logoEntreprendreEnsemble from "../assets/clients/logo_entreprendre_ensemble.webp";
+import logoEpideCambrai from "../assets/clients/logo_epide_cambrai.webp";
+import logoFondationDesPossibles from "../assets/clients/logo_fondation_des_possibles.webp";
+import logoFranceTravail from "../assets/clients/logo_france_travail.webp";
+import logoGeicqMobilite from "../assets/clients/logo_geicq_mobilite_nouvelle_aquitaine.webp";
+import logoGretaGrandArtois from "../assets/clients/logo_greta_grand_artois.webp";
+import logoGroupeM6 from "../assets/clients/logo_groupe_m6.webp";
+import logoIdrac from "../assets/clients/logo_idrac.webp";
+import logoLeclercTempleneuve from "../assets/clients/logo_leclerc_templeuve.webp";
+import logoLetudiant from "../assets/clients/logo_letudiant.webp";
+import logoMissionLocaleDenain from "../assets/clients/logo_mission_locale_denain.webp";
+import logoMissionLocaleValenciennes from "../assets/clients/logo_mission_locale_valenciennes.webp";
+import logoPartnaire from "../assets/clients/logo_partnaire.webp";
+import logoReussirEnSambre from "../assets/clients/logo_reussir_en_sambre_avesnois.webp";
+import logoSynergie from "../assets/clients/logo_synergie.webp";
+import logoVerinFormation from "../assets/clients/logo_verin_formation.webp";
+import logoYellowE from "../assets/clients/logo_yellow_e.webp";
 
 export const clientLogos = [
+  { name: "Actual", logo: logoActual },
+  { name: "Adélie", logo: logoAdelie },
+  { name: "AFPI Dunkerque", logo: logoAfpiDunkerque },
   { name: "Assifep", logo: logoAssifep },
+  { name: "CCI Grand Nancy Métropole", logo: logoCciGrandNancy },
   { name: "Direct Formation", logo: logoDirectFormation },
+  { name: "EFP Bruxelles", logo: logoEfpBruxelles },
+  { name: "Elev’up", logo: logoElevup },
+  { name: "Entreprendre Ensemble", logo: logoEntreprendreEnsemble },
+  { name: "EPIDE Cambrai", logo: logoEpideCambrai },
+  { name: "Fondation des Possibles", logo: logoFondationDesPossibles },
   { name: "France Travail", logo: logoFranceTravail },
-  { name: "Campus Pro", logo: campus },
-  { name: "Fun Radio", logo: logoFunRadio },
-  { name: "Groupe M6", logo: logoM6 },
-  { name: "Racing Club de Lens", logo: rcl },
-  { name: "Crit", logo: crit },
-  { name: "Elev’up", logo: elevup },
-  { name: "Partnaire", logo: partnaire },
-  { name: "Adélie", logo: adelie },
-  { name: "Leclerc Templeneuve", logo: leclerc },
-  { name: "Synergie", logo: synergie },
-  { name: "Actual", logo: actual },
-  { name: "Laho", logo: laho },
-
-  // { name: "OpenClassroom", logo: logoFranceTravail },
-  // { name: "Mairie de Provin", logo: logoFranceTravail },
-  // { name: "Entreprendre ensemble", logo: logoFranceTravail },
-  // { name: "EFP Bruxelles", logo: logoFranceTravail },
-  // { name: "Université polytechnique Hauts-de-France", logo: logoFranceTravail },
-  // { name: "GEIQ Mobilité Nouvelle Aquitaine", logo: logoFranceTravail },
-  // { name: "Réussir en Ambre Avesnois", logo: logoFranceTravail },
-  // { name: "L’Etudiant", logo: logoFranceTravail },
-  // { name: "Mission Locale Jeune de Valenciennes", logo: logoFranceTravail },
-  // { name: "Institution Saint Jean de Douai", logo: logoFranceTravail },
-  // { name: "Mission Locale Denain", logo: logoFranceTravail },
-  // { name: "Leader Ergalis", logo: logoFranceTravail },
-  // { name: "Idrac", logo: logoFranceTravail },
-  // { name: "La MEM", logo: logoFranceTravail },
-  // { name: "Verin Formation", logo: logoFranceTravail },
-  // { name: "Maisons & Cités", logo: logoFranceTravail },
-  // { name: "Sia Habitat", logo: logoFranceTravail },
-  // { name: "Festival Unique Paris", logo: logoFranceTravail },
-  // { name: "Greta Grand Artois", logo: logoFranceTravail },
-  // { name: "Fondation des Possibles", logo: logoFranceTravail },
-  // { name: "IJ HDF", logo: logoFranceTravail },
-  // { name: "EPIDE", logo: logoFranceTravail },
+  { name: "GEICQ Mobilité Nouvelle Aquitaine", logo: logoGeicqMobilite },
+  { name: "Greta Grand Artois", logo: logoGretaGrandArtois },
+  { name: "Groupe M6", logo: logoGroupeM6 },
+  { name: "IDRAC", logo: logoIdrac },
+  { name: "Leclerc Templeneuve", logo: logoLeclercTempleneuve },
+  { name: "L’Etudiant", logo: logoLetudiant },
+  { name: "Mission Locale Denain", logo: logoMissionLocaleDenain },
+  { name: "Mission Locale Valenciennes", logo: logoMissionLocaleValenciennes },
+  { name: "Partnaire", logo: logoPartnaire },
+  { name: "Réussir en Sambre Avesnois", logo: logoReussirEnSambre },
+  { name: "Synergie", logo: logoSynergie },
+  { name: "Verin Formation", logo: logoVerinFormation },
+  { name: "Yellow E", logo: logoYellowE },
 ];
